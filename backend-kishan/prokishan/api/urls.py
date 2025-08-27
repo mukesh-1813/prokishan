a4f61ced18860e6d052d50d0182e96e6a2526d2f
mukesh-1813 <mukeshbudi18@gmail.com>
2025-08-27 19:29:26 +0530

    Update urls.py

--- backend-kishan/prokishan/api/urls.py
+++ backend-kishan/prokishan/api/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
-from .views import RegisterView, LoginView  # Make sure you're importing the correct class names
+from .views import (
+    RegisterView,
+    LoginView,
+    CropListView,
+    CropDetailView,
+   SchemeListView,
+    SchemeDetailView,
+)
 
 urlpatterns = [
-    path('users/', RegisterView.as_view(), name='user-list-create'),
-    path('login/', LoginView.as_view(), name='user-login'),
+    # Auth
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+
+    # Crops
+    path("crops/", CropListView.as_view(), name="crop-list-create"),
+    path("crops/<int:pk>/", CropDetailView.as_view(), name="crop-detail"),
+
+    # Schemes
+    path("schemes/", SchemeListView.as_view(), name="scheme-list-create"),
+    path("schemes/<int:pk>/", SchemeDetailView.as_view(), name="scheme-detail"),
 ]
