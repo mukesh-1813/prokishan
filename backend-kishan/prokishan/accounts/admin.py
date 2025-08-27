44c869bfa738367af0ba165b8dc4e0c069269a63
mukesh-1813 <mukeshbudi18@gmail.com>
2025-08-27 15:29:44 +0530

    Update admin.py

--- backend-kishan/prokishan/accounts/admin.py
+++ backend-kishan/prokishan/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = ['id', 'phone_no', 'username', 'address', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('phone_no', 'username', 'address', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_no', 'username', 'address', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ['phone_no', 'username']
+    ordering = ['phone_no']
