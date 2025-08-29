0eac83afa98372b859de77394aa258b0f68da5e1
mukesh-1813 <mukeshbudi18@gmail.com>
2025-08-29 18:47:57 +0530

    Update App.jsx

--- frountend-kishan/Prokishan/src/App.jsx
+++ frountend-kishan/Prokishan/src/App.jsx
@@ -1,14 +1,25 @@
-import Navbar from './compnents/navbar'
-
+import Navbar from './compnents/Navbar';
+import Login from './compnents/Login';
+import Home from './compnents/Home';
+import Register from './compnents/register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
-
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
-  )
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Login Page */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Register Page */}
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
